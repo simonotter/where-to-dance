ee105243b22af28bdfd341506532e879e692bca9
Simon Otter <simonotter@gmail.com>
2020-12-10 11:23:25 +0000

    Added middy middleware and CORS headers

--- backend/src/lambda/http/createVenue.ts
+++ backend/src/lambda/http/createVenue.ts
@@ -1,14 +1,16 @@
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import 'source-map-support/register';
 import { getToken, parseUserId } from '../../auth/utils';
 import { createVenue } from '../../businessLogic/venues';
 import { Venue } from '../../models/Venue';
 import { CreateVenueRequest } from '../../requests/CreateVenueRequest';
 import { createLogger } from '../../utils/logger';
+import * as middy from 'middy';
+import { cors } from 'middy/middlewares';
 
 const logger = createLogger('createVenue');
 
-export const handler: APIGatewayProxyHandler = async (
+export const handler = middy(async (
 	event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   
 	logger.info('Processing event: ', { event: event });
@@ -34,6 +36,10 @@ export const handler: APIGatewayProxyHandler = async (
 		})
 	};
 
-};
+});
 
-// TODO: Add middy middleware and CORS headers.
+handler.use(
+	cors({
+		credentials: true
+	})
+);
